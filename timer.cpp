b0ac0754f3dbb89821dd743ec6c542a90281f955
cu-sp <down1995@tutanota.com>
2024-05-11 14:30:03 -0400

    Added timer func in timer.cpp
    currently has a 10s timer that can be adjustable.

--- timer.cpp
+++ timer.cpp
@@ -1,52 +1,17 @@
-#include <cerrno>
-#include <cstring>
-#include <iostream>
-#include <signal.h>
-#include <sys/time.h>
-#include <unistd.h>
+#include <chrono>   // Time library
+#include <cstdlib>  //unsure
+#include <iostream> //stdio
+#include <thread>   //generate threads
 
-void handleError(const char *msg) {
-  std::cout << msg << ": " << strerror(errno) << std::endl;
-  exit(EXIT_FAILURE);
+void timer(int duration) {
+  while (duration > 0) {
+    std::this_thread::sleep_for(
+        std::chrono::milliseconds(1000)); // sleep for 1 seconds
+    // do something here after every 1 seconds...
+    std::system("clear");
+    std::cout << duration << std::endl;
+    duration--;
+  };
 }
 
-void alarm_handler(int signum) {
-  // Do nothing; just handle the signal
-}
-
-int main() {
-  struct itimerval timer;
-
-  // Set Up the New timer
-  timer.it_value.tv_sec = 10;
-  timer.it_value.tv_usec = 0;
-  timer.it_interval.tv_sec = 1;
-  timer.it_interval.tv_usec = 0;
-
-  if (setitimer(ITIMER_REAL, &timer, nullptr) == -1) {
-    handleError("setitimer");
-  }
-
-  // Register the signal handler
-  if (signal(SIGALRM, alarm_handler) == SIG_ERR) {
-    handleError("signal");
-  }
-
-  // Loop until the timer expires
-  while (true) {
-    // Get the current timer value
-    if (getitimer(ITIMER_REAL, &timer) == -1) {
-      handleError("getitimer");
-    }
-
-    // Display the time remaining on the timer
-    std::cout << "Time remaining on timer: " << timer.it_value.tv_usec
-              << " microseconds " << timer.it_value.tv_sec << " seconds"
-              << std::endl;
-
-    // Pause execution until the next timer expiration
-    pause();
-  }
-
-  return 0;
-}
+int main() { timer(10); }
